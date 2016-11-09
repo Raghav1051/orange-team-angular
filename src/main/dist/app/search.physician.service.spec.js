90cba7c231ce8e14791e1baf8ae66b9537f7cba4
James Black <JamesB@emids.com>
2016-11-09 10:49:13 -0600

    Made changes to see if the test will pass

--- src/main/dist/app/search.physician.service.spec.js
+++ src/main/dist/app/search.physician.service.spec.js
@@ -3,13 +3,25 @@ var search_physician_service_1 = require("./search.physician.service");
 var searchmodel_1 = require("./searchmodel");
 describe('SerachPhysicianService Tests', function () {
     var service = new search_physician_service_1.SerachPhysicianService();
-    it('Should fail', function () {
-        expect(false).toBeTruthy();
-    });
-    it('Should return a list of doctors by last name', function () {
+    it('Should return an empty list of doctors', function () {
         var searchModel = new searchmodel_1.SearchModel("", "", "", "", "");
         var items = service.getSearchedPhysician(searchModel, false);
-        expect(items).toBeGreaterThan(5);
+        expect(items).toEqual(0);
+    });
+    it('Should return a list of doctors by last name', function () {
+        var searchModel = new searchmodel_1.SearchModel("", "", "", "Smith", "");
+        var items = service.getSearchedPhysician(searchModel, false);
+        expect(items).toBeGreaterThan(0);
+    });
+    it('Should return a list of doctors by specialty', function () {
+        var searchModel = new searchmodel_1.SearchModel("", "", "dermatology", "", "");
+        var items = service.getSearchedPhysician(searchModel, false);
+        expect(items).toBeGreaterThan(0);
+    });
+    it('Should return a list of doctors by specialty and last name', function () {
+        var searchModel = new searchmodel_1.SearchModel("", "", "dermatology", "smith", "");
+        var items = service.getSearchedPhysician(searchModel, false);
+        expect(items).toBeGreaterThan(0);
     });
 });
 //# sourceMappingURL=search.physician.service.spec.js.map
--- src/main/webapp/app/search.physician.service.spec.ts
+++ src/main/webapp/app/search.physician.service.spec.ts
@@ -4,15 +4,32 @@ import {SearchModel} from "./searchmodel"
 describe('SerachPhysicianService Tests', () => {
     let service:SerachPhysicianService = new SerachPhysicianService();
 
-    it('Should fail', () => {
-      expect(false).toBeTruthy();
-    });
-    
-    it('Should return a list of doctors by last name', () => {
+    it('Should return an empty list of doctors', () => {
 
       var searchModel:SearchModel = new SearchModel("","","","","");
         var items = service.getSearchedPhysician(searchModel, false);
 
-        expect(items).toBeGreaterThan(5);
+        expect(items).toEqual(0);
+    });
+    it('Should return a list of doctors by last name', () => {
+
+      var searchModel:SearchModel = new SearchModel("","","","Smith","");
+        var items = service.getSearchedPhysician(searchModel, false);
+
+        expect(items).toBeGreaterThan(0);
+    });
+    it('Should return a list of doctors by specialty', () => {
+
+      var searchModel:SearchModel = new SearchModel("","","dermatology","","");
+        var items = service.getSearchedPhysician(searchModel, false);
+
+        expect(items).toBeGreaterThan(0);
+    });
+    it('Should return a list of doctors by specialty and last name', () => {
+
+      var searchModel:SearchModel = new SearchModel("","","dermatology","smith","");
+        var items = service.getSearchedPhysician(searchModel, false);
+
+        expect(items).toBeGreaterThan(0);
     });
 });
