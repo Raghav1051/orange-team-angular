f8b517156c0e1b272caccabbbef0f5480446a593
James Black <JamesB@emids.com>
2016-11-09 08:11:36 -0600

    Removing files that were moved.

--- app/main.ts
+++
@@ -1,8 +0,0 @@
-
-/**
- * This the main component of the application
- */
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
