3df07d48dca34e72b00103700370a66e1a9d13b2
Raghavendra Hrushikesh <RaghavendraH@emids.com>
2016-11-11 13:03:32 -0600

    Test cases fro search component, more to come

---
+++ src/main/webapp/app/search.component.spec.ts
@@ -0,0 +1,26 @@
+import {SearchComponent} from './app.component.search';
+import {Specialities,SearchModel}
+import {TestBed, async} from 'from '@angular/core/testing';
+
+describe('SearchComponent Tests', async() =>{
+beforeEach(() => {
+  let seearchcomponent: SearchComponent = new SearchComponent();
+})
+
+
+it('should return the length of specialities' ,() =>{
+let spec[]: specialities[];
+expect(spec[].length).toEqual(12);
+expect(spec[]).toEqual(["PHYSICAL THERAPY", "CHIROPRACTIC", "OCCUPATIONAL THERAPY", "CLINICAL SOCIAL WORKER", "DERMATOLOGY","PEDIATRIC EYE ASSOCIATES INC", "PODIATRY", "PERIPHERAL VASCULAR DISEASE", "PHYSICAL THERAPY","DIAGNOSTIC RADIOLOGY", "OPTOMETRY", "NUCLEAR MEDICINE"])
+});
+
+//it('should return the results on hitting Submit' ,() =>{
+//let advsearch: advancesearch
+  //expect(onSubmit().advancesearch)
+//}
+
+it('should make all fields null after reset', () =>{
+var searchModel:SearchModel = new SearchModel(null,null,null,null,null);
+expect(reset.searchModel).toBeNull();
+});
+s});
